3567da26b3df6a856d375fe0825fe935a9027b3e
joaoc <joao.carabetta@gmail.com>
2017-12-04 19:08:00 -0200

    minor tweaks

--- main_options/fluxo_tramitacao/callback.py
+++ main_options/fluxo_tramitacao/callback.py
@@ -1,4 +1,21 @@
 def draw_plot_1(input1, input2):
     return u'Input 1 is "{}" and Input 2 is OVO'.format(input1, input2)
 
-fluxo_tramitacao = {'draw_plot_1': draw_plot_1}
+
+def draw_plot_2(input1, input2):
+    pass
+
+def draw_plot_diff(input1, input2):
+    pass
+
+def big_numbers_1(input1, input2):
+    pass
+
+def big_numbers_2(input1, input2):
+    pass
+
+fluxo_tramitacao = {"draw_plot_1": draw_plot_1,
+                    "draw_plot_2": draw_plot_2,
+                    "draw_plot_diff": draw_plot_diff,
+                    "big_numbers_1": big_numbers_1,
+                    "big_numbers_2": big_numbers_2}
