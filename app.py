1cc5991b03deac4226372dba7256f71c69417b01
joaoc <joao.carabetta@gmail.com>
2017-12-05 12:43:13 -0200

    starting test

--- app.py
+++ app.py
@@ -49,133 +49,45 @@ app.layout = html.Div([
 
    html.Hr(),
 
-# (2) Seleção gráfico + instância
    html.Div([
+       dcc.Dropdown(
+           id='choice',
+           options=[{'label': variables,
+                     'value': [0,1]}])]
 
-           html.Div([
-
-               html.P('Selecione o gráfico a ser mostrado:'),
-               dcc.RadioItems(
-                   id='graph-selector',
-                   options=[{'label': option['full_name'],
-                             'value': option['back_name']}
-                            for option in options_properties],
-                   value=options_properties[0]['back_name'],
-                   labelStyle={'display': 'inline-block'}
-                    )
-                ],
-               className='six columns'
-           ),
-
-           html.Div([
-
-               html.P('Selecione a instância a ser comparada:'),
-               dcc.RadioItems(
-                   id='compare-selector',
-                   options=[{'label': option['full_name'],
-                             'value': option['back_name']}
-                            for option in options_properties],
-                   labelStyle={'display': 'inline-block'}
-                   )
-               ],
-               className='six columns'
-                )
-            ],
-            className='row'
+            className='ten columns offset-by-one'
         ),
 
-    html.Hr(),
-
-# (3) Seleção da comparação
-    html.Div([
-
-            html.P('Selecione os valores para comparação:',
-                   style={'margin-top': '20'})
-
-    ]),
-
-    html.Div([
-
-           html.Div([
-               dcc.Dropdown(
-                   id='compare-values',
-                   options=[{'label': option['full_name'],
-                             'value': option['back_name']}
-                            for option in options_properties]
-                    )
-                ],
-               className='four columns'
-           ),
-
-           html.Div([
-
-               dcc.Dropdown(
-                   id='xaxis-type',
-                   options=[{'label': option['full_name'],
-                             'value': option['back_name']}
-                            for option in options_properties]
-                    )
-                ],
-               className='four columns'
-                )
-            ],
-            className='row'
+   html.Div(id='menu',
+            className='ten columns offset-by-one'
         ),
-
-# (4) Gráficos
-    html.Div(
-        [
-            html.Div(
-                [
-                    dcc.Graph(id='plot_1')
-                ],
-                className='six columns',
-                style={'margin-top': '20'}
-            ),
-            html.Div(
-                [
-                    dcc.Graph(id='plot_2')
-                ],
-                className='six columns',
-                style={'margin-top': '20'}
-            ),
-        ],
-        className='row'
-    ),
-
-    html.Div(
-        [
-            html.Div(
-                html.H5('Algo interessante para'
-                        'ser colocado aqui'
-                        ''
-                        '\n Um número bonito:'
-                        '\n 13718 coisas'),
-                className='three columns',
-                style={'margin-top': '40'}
-            ),
-            html.Div(
-                [
-                    dcc.Graph(id='plot_diff')
-                ],
-                className='six columns',
-                style={'margin-top': '40'}
-            ),
-            html.Div(
-                html.H5('Hnm... Muito interessante'
-                        ''
-                        '\n Outro número bonito: '
-                        '\n 1823 coisas'),
-                className='three columns',
-                style={'margin-top': '40'}
-            ),
-        ],
-        className='row'
-    ),
-
-],
-    className='ten columns offset-by-one'
+    ]
 )
+
+@app.callback(Output('output', 'children'),
+              [Input('input-1', 'value')])
+def update_menu():
+
+    menus = []
+
+    for variables in options_properties[0]['variables']:
+
+        menus.append(dcc.Dropdown(
+                   id='{}'.format(variables),
+                   options=[{'label': variables,
+                             'value': [0,1]}]
+                    )
+                )
+
+
+#@app.callback(Output('output', 'children'),
+#              [Input('input-1', 'value'),
+#               Input('input-2', 'value'),
+#               Input('xaxis-type', 'value')])
+#def draw_plot_1(input1, input2, input3):
+#
+#    func = options_functions[input3]['draw_plot_1']
+#    return func(input1, input2)
 
 
 
