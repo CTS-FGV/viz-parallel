9d61b04443da104366def0b0cb7fb2948128dd2d
Fernanda SCovino <fscovinom@gmail.com>
2017-12-13 16:54:59 -0200

    changing colorscale + hovertext heatmap

--- plots/perfil_tempo_tramitacao/plot.py
+++ plots/perfil_tempo_tramitacao/plot.py
@@ -5,11 +5,10 @@ import imp
 
 raw_data = imp.load_source('info', 'plots/perfil_tempo_tramitacao/get_raw_data.py').output['raw_data']
 
-#org = ['Administrativa', 'Comissões Temáticas', 'Constituição e Justiça',
-#       'Mesa', 'Plenário', 'Temporárias', 'Tributação e Finanças']
-
 org = ['Administrativa', 'Comissões Temáticas', 'Temporárias',
        'Constituição e Justiça', 'Tributação e Finanças', 'Mesa', 'Plenário']
+
+hm_col = ['#caf1f7','#93ecf9','#76d2e0','#51bccc','#078699','#0a5c68','#04333a','#001d21','#001f23','#00090a']
 
 bins = np.linspace(0, 300, 31)
 
@@ -18,43 +17,49 @@ def plot(input, raw_data, filtered_data):
     status = input['situacao-perfil']
     periodo = input['tempo-perfil']
 
-    return prepare_plot(raw_data, filtered_data, status, org, bins, periodo)
+    temp = prepare_data(raw_data, filtered_data, status, periodo)
+
+    x = bins
+    y = temp.columns.values
+    z = temp.values.T
+    #zmax = round(z[:-1].max())
+
+    return draw_plot(x, y, z)
 
 
-def draw_plot(x, y, z, hm_col):
+def draw_plot(x, y, z):
+
+    #colorscale
+    max = 8
+    # max = int(zmax / 10)
+
+    colorscale = [[0, hm_col[0]]]
+
+    for i in range(0, max):
+        colorscale.append([i / max, hm_col[i]])
+        colorscale.append([(i + 1) / max, hm_col[i]])
+
+    #hovertext
+    hovertext = list()
+    for yi, yy in enumerate(y):
+       hovertext.append(list())
+       for xi, xx in enumerate(x):
+           if xi < len(z[yi]):
+               xx = int(xx)
+               hovertext[-1].append(
+                   'Dos PLs tramitados de {} a {} meses na Câmara,<br>'.format(xx, xx + 10) + ' {0:.3f}% '.format(
+                       z[yi][xi]) + 'do tempo foi em {}'.format(yy))
+           else:
+               hovertext[-1].append('Não houveram PLs tramitados na Câmara por {} meses'.format(xx))
+
     trace = [go.Heatmap(z=z,
                         y=y,
                         x=x,
-                        # text=hovertext,
-                        # hoverinfo='text',
-                        colorscale=[
-
-                            [0, hm_col[6]],
-                            [1 / 7, hm_col[6]],
-
-                            # Let values between 10-20% of the min and max of z
-                            # have color rgb(20, 20, 20)
-                            [1 / 7, hm_col[5]],
-                            [2 / 7, hm_col[5]],
-
-                            # Values between 20-30% of the min and max of z
-                            # have color rgb(40, 40, 40)
-                            [2 / 7, hm_col[4]],
-                            [3 / 7, hm_col[4]],
-
-                            [3 / 7, hm_col[3]],
-                            [4 / 7, hm_col[3]],
-
-                            [4 / 7, hm_col[2]],
-                            [5 / 7, hm_col[2]],
-
-                            [5 / 7, hm_col[1]],
-                            [6 / 7, hm_col[1]],
-
-                            [6 / 7, hm_col[0]],
-                            [1.0, hm_col[0]]],
+                        text=hovertext,
+                        hoverinfo='text',
+                        colorscale=colorscale,
                         zmin=0,
-                        zmax=70,
+                        zmax=80,
                         colorbar=go.ColorBar(title='Tempo no órgão (%)', titleside='top'),
                         showscale=True)]
 
@@ -67,14 +72,11 @@ def draw_plot(x, y, z, hm_col):
 
 
 def prepare_data(raw_data, filtered_data, status=None, periodo=None):
+
     if status:
-        # df = raw_data[raw_data['situacao_tipo'] == status]
-        # df['dataInicio'] = pd.to_datetime(df['dataInicio'])
-        # df = df[df['dataInicio'] >= str(periodo[0])]
-        # df = df[df['dataInicio'] <= str(periodo[1])]
         df = filtered_data
         print('FILTEDER!!!!!!')
-        #print(filtered_data)
+
     else:
         df = raw_data
 
@@ -103,33 +105,6 @@ def prepare_data(raw_data, filtered_data, status=None, periodo=None):
     temp = temp.fillna(0)
 
     return temp
-
-
-# Passar para dentro de draw_plot_1
-def prepare_plot(raw_data, filtered_data, status, org, bins, periodo):
-    temp = prepare_data(raw_data, filtered_data, status, periodo)
-
-    x = bins
-    y = temp.columns.values
-    z = temp.values.T
-
-    #print(list(temp.columns.values))
-
-    # hovertext = list()
-    # for yi, yy in enumerate(y):
-    #    hovertext.append(list())
-    #    for xi, xx in enumerate(x):
-    #        if xi < len(z1[yi]):
-    #            xx = int(xx)
-    #            hovertext[-1].append(
-    #                'Dos PLs tramitados de {} a {} meses na Câmara,<br>'.format(xx, xx + 10) + ' {0:.3f}% '.format(
-    #                    z1[yi][xi]) + 'do tempo foi em {}'.format(yy))
-    #        else:
-    #            hovertext[-1].append('Não houveram PLs tramitados na Câmara por {} meses'.format(xx))
-
-    hm_col = ['#450A5C', '#423C81', '#34608C', '#2583A5', '#61B7C7', '#75D2DA', '#C1E1EA']
-
-    return draw_plot(x, y, z, hm_col)
 
 
 output = {'plot': plot}
